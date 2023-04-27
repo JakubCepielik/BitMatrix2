c4f3d26d1771d3627eadeee1e3f79cf36b7ec65c
Jakub Cepielik <jakubcepielik168@gmail.com>
2023-04-27 15:15:04 +0200

    Dodaj pliki projektów.

---
+++ BitMatrix2/BitMatrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitMatrix2
+{
+
+    // prostokątna macierz bitów o wymiarach m x n
+    public partial class BitMatrix
+    {
+        private BitArray data;
+        public int NumberOfRows { get; }
+        public int NumberOfColumns { get; }
+        public bool IsReadOnly => false;
+
+        // tworzy prostokątną macierz bitową wypełnioną `defaultValue`
+        public BitMatrix(int numberOfRows, int numberOfColumns, int defaultValue = 0)
+        {
+            if (numberOfRows < 1 || numberOfColumns < 1)
+                throw new ArgumentOutOfRangeException("Incorrect size of matrix");
+            data = new BitArray(numberOfRows * numberOfColumns, BitToBool(defaultValue));
+            NumberOfRows = numberOfRows;
+            NumberOfColumns = numberOfColumns;
+        }
+
+        public static int BoolToBit(bool boolValue) => boolValue ? 1 : 0;
+        public static bool BitToBool(int bit) => bit != 0;
+        public override string ToString()
+        {
+            string wyswietl = "";
+            for(int i = 0; i<NumberOfRows*NumberOfColumns; i++) 
+            { 
+                if((i+1)%NumberOfColumns == 0)
+                {
+                    wyswietl += BoolToBit(data[i]).ToString();
+                    wyswietl += "\n";
+                }
+                else
+                wyswietl += BoolToBit(data[i]).ToString();
+            }
+            return wyswietl;
+        }
+    }
+
+
+    
+
+}
---
+++ BitMatrix2/Program.cs
@@ -0,0 +1,7 @@
+﻿using BitMatrix2;
+
+
+var m = new BitMatrix(2, 4);
+var n = new BitMatrix(1, 1, 1);
+Console.WriteLine(m);
+Console.WriteLine(n);
