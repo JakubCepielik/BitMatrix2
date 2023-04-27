852af0558cf397205a32f773cb9c246603a24946
Jakub Cepielik <jakubcepielik168@gmail.com>
2023-04-27 22:05:12 +0200

    Added indexer, iterator

--- BitMatrix2/Program.cs
+++ BitMatrix2/Program.cs
@@ -9,14 +9,18 @@ m2 = new BitMatrix(1, 1, 1);
 Console.WriteLine(m1.Equals(m2));
 */
 
-var m1 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
-var m2 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
-Console.WriteLine(m1 != m2);
-Console.WriteLine(m1 == m2);
-Console.WriteLine(m2 != m1);
-Console.WriteLine(m2 == m1);
-
-m1 = new BitMatrix(1, 1, 1);
-m2 = new BitMatrix(1, 1, 1);
-Console.WriteLine(m1 == m2);
-Console.WriteLine(m1 != m2);
+int[] arr = new int[] { -1, 1, 3, 4 };
+foreach (var i in arr)
+    foreach (var j in arr)
+    {
+        var m = new BitMatrix(3, 4);
+        try
+        {
+            m[i, j] = 1;
+            Console.WriteLine($"m[{i}, {j}] = {m[i, j]}");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine($"m[{i}, {j}] = exception");
+        }
+    }
---
+++ BitMatrix2/indexer_iterator.cs
@@ -0,0 +1,41 @@
+﻿namespace BitMatrix2
+{
+
+    using System;
+    using System.Collections;
+
+    public partial class BitMatrix : IEnumerable<int>
+    {
+        public int this[int i, int j]
+        {
+            get 
+            { 
+                if (NumberOfRows <= i || NumberOfColumns <= j || i < 0 || j < 0) throw new IndexOutOfRangeException();
+                else
+                    return BoolToBit(data[i * NumberOfColumns + j]);
+            }
+            set 
+            {
+                if (NumberOfRows <= i || NumberOfColumns <= j || i <0 || j<0) throw new IndexOutOfRangeException();
+                else
+                data[i * NumberOfColumns + j] = BitToBool(value); 
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for(int i = 0; i<data.Length;i++)
+            {
+                
+                yield return BoolToBit(data[i]);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+}
+
