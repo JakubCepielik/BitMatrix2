9657e6e9640f954b2e0fb8cfbef474732dc8f10c
Jakub Cepielik <jakubcepielik168@gmail.com>
2023-04-27 22:35:20 +0200

    Added ICloneable interface

---
+++ BitMatrix2/Clone.cs
@@ -0,0 +1,21 @@
+﻿namespace BitMatrix2
+{
+    using System;
+    using System.Collections;
+
+    public partial class BitMatrix : ICloneable
+    {
+        public object Clone()
+        {
+            BitMatrix copy = new BitMatrix(this.NumberOfRows,this.NumberOfColumns);
+            int i = 0;
+            foreach(bool item in this.data) 
+            {
+                copy.data[i] = item;
+                i++;
+            }
+            return copy;
+        }
+    }
+
+}
--- BitMatrix2/Program.cs
+++ BitMatrix2/Program.cs
@@ -1,26 +1,6 @@
 ﻿using BitMatrix2;
-
-/*
-var m1 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
-var m2 = new BitMatrix(2, 3, 0, 0, 0, 1, 1, 1);
-Console.WriteLine(m1.Equals(m2));
-m1 = new BitMatrix(1, 1, 0);
-m2 = new BitMatrix(1, 1, 1);
-Console.WriteLine(m1.Equals(m2));
-*/
-
-int[] arr = new int[] { -1, 1, 3, 4 };
-foreach (var i in arr)
-    foreach (var j in arr)
-    {
-        var m = new BitMatrix(3, 4);
-        try
-        {
-            m[i, j] = 1;
-            Console.WriteLine($"m[{i}, {j}] = {m[i, j]}");
-        }
-        catch (IndexOutOfRangeException)
-        {
-            Console.WriteLine($"m[{i}, {j}] = exception");
-        }
-    }
+int[] arr = new int[6] { 1, 0, 1, 1, 0, 1 };
+var m = new BitMatrix(2, 3, arr);
+var m1 = (BitMatrix)(m.Clone());
+m[0, 1] = 1;
+Console.WriteLine(m[0, 1] != m1[0, 1]);
