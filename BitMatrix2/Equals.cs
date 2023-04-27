7a4271e662d430cb902564c63f8ba369917d0739
Jakub Cepielik <jakubcepielik168@gmail.com>
2023-04-27 21:08:03 +0200

    Added IEquatable interface

---
+++ BitMatrix2/Equals.cs
@@ -0,0 +1,45 @@
+﻿namespace BitMatrix2
+{
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public partial class BitMatrix : IEquatable<BitMatrix>
+    {
+        public bool Equals(BitMatrix other)
+        {
+            if ((object)other == null) return false;
+            if (this.NumberOfColumns == other.NumberOfColumns && this.NumberOfRows == other.NumberOfRows && this.data.Length == other.data.Length)
+            {
+                for(int i=0; i<this.data.Length; i++)
+                {
+                    if (this.data[i] != other.data[i]) return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is BitMatrix) return Equals((BitMatrix)obj);
+            else return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(data, NumberOfRows, NumberOfColumns);
+        }
+
+        public static bool operator ==(BitMatrix left, BitMatrix right)
+        {
+            return EqualityComparer<BitMatrix>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(BitMatrix left, BitMatrix right)
+        {
+            return !(left == right);
+        }
+    }
+}
--- BitMatrix2/Program.cs
+++ BitMatrix2/Program.cs
@@ -1,5 +1,22 @@
 ﻿using BitMatrix2;
 
+/*
+var m1 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
+var m2 = new BitMatrix(2, 3, 0, 0, 0, 1, 1, 1);
+Console.WriteLine(m1.Equals(m2));
+m1 = new BitMatrix(1, 1, 0);
+m2 = new BitMatrix(1, 1, 1);
+Console.WriteLine(m1.Equals(m2));
+*/
 
-var m = new BitMatrix(2, 2, null);
-Console.WriteLine(m);
+var m1 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
+var m2 = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
+Console.WriteLine(m1 != m2);
+Console.WriteLine(m1 == m2);
+Console.WriteLine(m2 != m1);
+Console.WriteLine(m2 == m1);
+
+m1 = new BitMatrix(1, 1, 1);
+m2 = new BitMatrix(1, 1, 1);
+Console.WriteLine(m1 == m2);
+Console.WriteLine(m1 != m2);
