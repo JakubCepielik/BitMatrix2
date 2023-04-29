5d671a4925424be873f4e8601527c678d8e6696d
Jakub Cepielik <jakubcepielik168@gmail.com>
2023-04-29 16:01:39 +0200

    Added Bit Operators

---
+++ BitMatrix2/BitOperations.cs
@@ -0,0 +1,102 @@
+﻿namespace BitMatrix2
+{
+    using System;
+    using System.Collections;
+    using System.Numerics;
+
+    public partial class BitMatrix
+    {
+        public BitMatrix And(BitMatrix other)
+        {
+            if (other == null) 
+                throw new ArgumentNullException();
+            if (NumberOfColumns != other.NumberOfColumns || NumberOfRows != other.NumberOfRows) 
+                throw new ArgumentException();
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] &= other.data[i];
+
+            return this;
+        }
+
+        public BitMatrix Or(BitMatrix other)
+        {
+            if (other == null)
+                throw new ArgumentNullException();
+            if (NumberOfColumns != other.NumberOfColumns || NumberOfRows != other.NumberOfRows)
+                throw new ArgumentException();
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] |= other.data[i];
+
+            return this;
+        }
+
+        public BitMatrix Xor(BitMatrix other)
+        {
+            if (other == null)
+                throw new ArgumentNullException();
+            if (NumberOfColumns != other.NumberOfColumns || NumberOfRows != other.NumberOfRows)
+                throw new ArgumentException();
+
+            for (int i = 0; i < data.Length; i++)
+                data[i] ^= other.data[i];
+
+            return this;
+        }
+
+        public BitMatrix Not()
+        {
+            if (this == null)
+                throw new ArgumentNullException();
+            for (int i = 0; i < data.Length; i++)
+                data[i] = !data[i];
+
+            return this;
+        }
+
+        public static BitMatrix operator &(BitMatrix left, BitMatrix right)
+        {
+            if (left == null || right ==  null)
+                throw new ArgumentNullException();
+            if (left.NumberOfColumns != right.NumberOfColumns || left.NumberOfRows != right.NumberOfRows)
+                throw new ArgumentException();
+            BitMatrix B = new BitMatrix(left.NumberOfRows, left.NumberOfColumns);
+            for (int i = 0; i < left.data.Length; i++)
+                B.data[i] = (left.data[i] &= right.data[i]);
+            return B; 
+        }
+
+
+        public static BitMatrix operator |(BitMatrix left, BitMatrix right)
+        {
+            if (left == null || right == null)
+                throw new ArgumentNullException();
+            if (left.NumberOfColumns != right.NumberOfColumns || left.NumberOfRows != right.NumberOfRows)
+                throw new ArgumentException();
+            BitMatrix B = new BitMatrix(left.NumberOfRows, left.NumberOfColumns);
+            for (int i = 0; i < left.data.Length; i++)
+                B.data[i] = (left.data[i] |= right.data[i]);
+            return B;
+        }
+
+
+        public static BitMatrix operator ^(BitMatrix left, BitMatrix right)
+        {
+            if (left == null || right == null)
+                throw new ArgumentNullException();
+            if (left.NumberOfColumns != right.NumberOfColumns || left.NumberOfRows != right.NumberOfRows)
+                throw new ArgumentException();
+            BitMatrix tmp = (BitMatrix)left.Clone();
+            BitMatrix B = new BitMatrix(left.NumberOfRows, left.NumberOfColumns);
+            for (int i = 0; i < left.data.Length; i++)
+                B.data[i] = (tmp.data[i] ^= right.data[i]);
+            return B;
+        }
+    }
+}
+//1 0 1
+//1 1 0
+
+//1 1 0
+//1 1 0
--- BitMatrix2/Program.cs
+++ BitMatrix2/Program.cs
@@ -1,15 +1,73 @@
 ﻿using BitMatrix2;
+using System.Collections;
 
 
-// konwersja jawna z `bool[,]` na `BitMatrix`
-// dane poprawne
-string s = @"011
-101
-";
-bool[,] a = new bool[,] {
-{ false, true, true },
-{ true, false, true } };
-var m = (BitMatrix)a;
-Console.WriteLine(2 == m.NumberOfRows);
-Console.WriteLine(3 == m.NumberOfColumns);
-Console.WriteLine(s == m.ToString());
+// operator ^
+// poprawne dane
+var m1 = new BitMatrix(2, 3, 1, 0, 1, 0, 1, 0);
+var m2 = new BitMatrix(2, 3, 0, 1, 0, 0, 1, 0);
+//czy ^ jest symetryczny
+Console.WriteLine(m1 ^ m2);
+Console.WriteLine(m2 ^ m1);
+if ((m1 ^ m2).Equals(m2 ^ m1))
+    Console.WriteLine("Correct data, symmetry: Pass");
+
+//czy wykonany poprawnie ^
+var expected = new BitMatrix(2, 3, 1, 1, 1, 0, 0, 0);
+var m3 = m1 ^ m2;
+if (expected.Equals(m3))
+    Console.WriteLine("Correct data: Pass");
+
+//czy wynik jest niezależną kopią
+if (!ReferenceEquals(m1, m3) && !ReferenceEquals(m2, m3))
+    Console.WriteLine("Correct data, ReferenceEquals: Pass");
+m1[1, 2] = 1; Console.WriteLine(m1[1, 2] != m3[1, 2]);
+
+// argument `null ^ null`
+try
+{
+    var m = (BitMatrix)null ^ (BitMatrix)null;
+    Console.WriteLine(m);
+    Console.WriteLine("Arguments null: Fail");
+}
+catch (ArgumentNullException)
+{
+    Console.WriteLine("Arguments null: Pass");
+}
+
+// right argument `null`
+try
+{
+    var m = (BitMatrix)null ^ new BitMatrix(2, 2);
+    Console.WriteLine(m);
+    Console.WriteLine("Right argument null: Fail");
+}
+catch (ArgumentNullException)
+{
+    Console.WriteLine("Right argument null: Pass");
+}
+
+// left argument `null`
+try
+{
+    var m = new BitMatrix(2, 2) ^ (BitMatrix)null;
+    Console.WriteLine(m);
+    Console.WriteLine("Left argument null: Fail");
+}
+catch (ArgumentNullException)
+{
+    Console.WriteLine("Left argument null: Pass");
+}
+
+// incorrect dimensions
+try
+{
+    var m = new BitMatrix(2, 3) ^ new BitMatrix(2, 2);
+    Console.WriteLine(m);
+    Console.WriteLine("Incorrect dimensions: Fail");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("Incorrect dimensions: Pass");
+}
+
